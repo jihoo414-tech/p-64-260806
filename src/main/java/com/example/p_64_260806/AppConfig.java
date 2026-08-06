7669539662423b298c05d55982321500396e7669
jihoo414-tech <jihoo414@gmail.com>
2026-08-06 12:03:47 +0900

    refactor files

--- src/main/java/com/example/p_64_260806/AppConfig.java
+++ src/main/java/com/example/p_64_260806/AppConfig.java
@@ -1,55 +1,33 @@
 package com.example.p_64_260806;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 public class AppConfig {
 
-//    @Bean
-//    public PersonService personService(){
-//        System.out.println("AppConfig.personService 호출됨!");
-//        return new PersonService();
-//    }
-
-
-    //어떤 객체는 초기화 작업을 복잡하게 해야할때도 있다.
-    //이럴때 AppConfig를 통해 빈을 주입하면 좋음
-    //만약 이렇게 하지않고 단순하게 빈을 등록기만 하면 되면 @Component같은 방식을 쓰자
-    @Bean
-    public PersonRepository personRepository() {
-        return new PersonRepository(1);
-    }
+    @Autowired
+    @Lazy
+    private AppConfig self;
 
     @Bean
-    public PersonRepository personRepositoryV2() {
-        return new PersonRepository(2);
-    }
-
-
-    @Bean
-    @Order
-    public ApplicationRunner myApplicationRunner2() {
+    public ApplicationRunner myApplicationRunner3() {
         return args -> {
-            work1();
-            work2();
+               self.work1();
+               self.work2();
         };
     }
-
+    @Transactional
     public void work1() {
         System.out.println("work1");
     }
 
+    @Transactional
     public void work2() {
         System.out.println("work2");
-    }
-
-    @Bean
-    @Order(1)
-    public ApplicationRunner myApplicationRunner1(){
-        System.out.println("myApplicationRunner1");
-        return new MyApplicationRunner(1);
     }
 }
