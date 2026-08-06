b461ff2300be54145b209bb899c59e779590e09c
jihoo414-tech <jihoo414@gmail.com>
2026-08-06 11:25:35 +0900

    add personRepository version

--- src/main/java/com/example/p_64_260806/AppConfig.java
+++ src/main/java/com/example/p_64_260806/AppConfig.java
@@ -6,9 +6,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+//    @Bean
+//    public PersonService personService(){
+//        System.out.println("AppConfig.personService 호출됨!");
+//        return new PersonService();
+//    }
+
+
+    //어떤 객체는 초기화 작업을 복잡하게 해야할때도 있다.
+    //이럴때 AppConfig를 통해 빈을 주입하면 좋음
+    //만약 이렇게 하지않고 단순하게 빈을 등록기만 하면 되면 @Component같은 방식을 쓰자
     @Bean
-    public PersonService personService(){
-        System.out.println("AppConfig.personService 호출됨!");
-        return new PersonService();
+    public PersonRepository personRepository() {
+        return new PersonRepository(1);
+    }
+
+    @Bean
+    public PersonRepository personRepositoryV2() {
+        return new PersonRepository(2);
     }
 }
--- src/main/java/com/example/p_64_260806/PersonService.java
+++ src/main/java/com/example/p_64_260806/PersonService.java
@@ -1,7 +1,18 @@
 package com.example.p_64_260806;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    //Qualifier를 통해 어떤 메서드를 통해 빈을 주입받을 것인지 선택 가능
+    public PersonService(@Qualifier("personRepository") PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
     public int count() {
-        return 3;
+        return personRepository.count();
     }
 }
