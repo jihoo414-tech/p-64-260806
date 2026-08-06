c42b7c0b61971187eceb3e84629198393cdf9b50
jihoo414-tech <jihoo414@gmail.com>
2026-08-06 10:48:59 +0900

    initial commit

---
+++ src/main/java/com/example/p_64_260806/P64260806Application.java
@@ -0,0 +1,13 @@
+package com.example.p_64_260806;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P64260806Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P64260806Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/p_64_260806/P64260806ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p_64_260806;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P64260806ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
